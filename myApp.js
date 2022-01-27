70dcd87d837388db17ef2d0b12f8da76f26ca6d7
Apinop <apinop.soisuwan@gmail.com>
2022-01-27 13:23:54 +0700

    adding dotenv

---
+++ myApp.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+require('dotenv').config();
+
+/** Start a Working Express Server */
+/**app.get("/",function(request,respone){
+  respone.send('Hello Express')
+})*/
+
+/** Serve an HTML File */
+var html_path =  __dirname + "/views/index.html"
+app.get("/",function(request,response){
+  response.sendFile(html_path)
+})
+
+
+/** Serve Static Assets */
+app.use('/public',express.static(__dirname + '/public'))
+
+
+/** Serve JSON on a Specific Route */
+/**app.get("/json",(request,response) => {
+  respone.json({message: "Hello json"})
+}) 
+
+
+
+//** Use the .env File */
+const mySecret = process.env['MESSAGE_STYLE']
+var message = {'message': "Hello json"} 
+app.get('/json',(request,response) => {
+    if (mySecret === "uppercase") {
+    response.json({'message': "HELLO JSON" });
+  } else {
+    response.json(message);
+  }
+})
+
+
+
+ module.exports = app;
