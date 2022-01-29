a75f93633533da2ef8d849c6a6747b2992973132
Apinop <apinop.soisuwan@gmail.com>
2022-01-29 15:30:48 +0700

    done with Route Parameter and Adding date:time

--- myApp.js
+++ myApp.js
@@ -2,10 +2,18 @@ var express = require('express');
 var app = express();
 require('dotenv').config();
 
+
+
 /** Start a Working Express Server */
 /**app.get("/",function(request,respone){
   respone.send('Hello Express')
 })*/
+
+/** Implement a Root-Level Request Logger Middleware  */
+app.use(function(req , res , next) {
+  console.log(req.method + " " + req.path + " - " + req.ip)
+  next()
+})
 
 /** Serve an HTML File */
 var html_path =  __dirname + "/views/index.html"
@@ -33,6 +41,24 @@ app.get('/json',(request,response, next) => {
     response.json(jsonResponse);
   }
     response.json(message);
+})
+
+
+/** Chain Middleware to Create a Time Server  */
+app.get('/now', function( req, res, next){
+  req.time = new Date().toString()
+  next();
+},  function( req, res){
+  res.send({time: req.time});
+});
+
+/** Get Route Parameter Input from the Client */
+
+app.get('/:word/echo', function(req ,res ){
+  var { word} = req.params;
+  res.json({
+    echo : word
+  })
 })
 
 
